53f2a5f4bb9f9ceeeadc8acebce729083c192479
Shakhrii <sh.ramaldanova@gmail.com>
2026-03-06 17:41:56 +0300

    feat: add card

---
+++ cards.js
@@ -0,0 +1,34 @@
+export const cards =  
+  [
+    {
+      "title": "Стальной канат для лифта GRS 9X21F(9X17)-IWRC 1570",
+      "amount": 50,
+      "image": "../assets/images/cards/card_1.svg"
+      
+    },
+     {
+      "title": "Плоский тяговой ремень для лифта GRS AAA717X1 (12 кордов)",
+      "amount": 0,
+      "image": "../assets/images/cards/card_2.svg"
+    },
+     {
+      "title": "Стальной канат для лифта GRS 8X19S-NFC 1570",
+      "amount": 50,
+      "image": "../assets/images/cards/card_3.svg"
+    },
+     {
+      "title": "Стальной канат для лифта GRS 9X21F(9X17)-IWRC 1570",
+      "amount": 50,
+      "image": "../assets/images/cards/card_1.svg"
+    },
+     {
+      "title": "Стальной канат для лифта GRS 8X19W-CWC 1570",
+      "amount": 50,
+      "image": "../assets/images/cards/card_4.svg"
+    },
+     {
+      "title": "Купить плоский тяговый ремень для лифта GRS AAA717AJ1(8 кордов)",
+      "amount": 50,
+      "image": "../assets/images/cards/card_2.svg"
+    }
+  ]
---
+++ js/catalog.js
@@ -0,0 +1,8 @@
+import { cards } from "../cards";
+
+const catalogList = document.querySelector('catalog-list');
+const drawCard = (card) => {
+  let cardItem = document.createElement('li');
+  
+
+}
