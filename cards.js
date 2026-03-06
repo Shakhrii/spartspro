794cc4f58f626a4156a72ca9d618597998e4feb3
Shakhrii <sh.ramaldanova@gmail.com>
2026-03-07 00:00:31 +0300

    feat: add page for 1920px

--- cards.js
+++ cards.js
@@ -3,32 +3,37 @@ export const cards =
     {
       "title": "Стальной канат для лифта GRS 9X21F(9X17)-IWRC 1570",
       "amount": 50,
-      "image": "../assets/images/cards/card_1.svg"
-      
+      "image": "../../assets/images/cards/card_1.svg",
+      "alt": "elevator rope" 
     },
      {
       "title": "Плоский тяговой ремень для лифта GRS AAA717X1 (12 кордов)",
       "amount": 0,
-      "image": "../assets/images/cards/card_2.svg"
+      "image": "../../assets/images/cards/card_2.svg",
+      "alt": "elevator belt" 
     },
      {
       "title": "Стальной канат для лифта GRS 8X19S-NFC 1570",
       "amount": 50,
-      "image": "../assets/images/cards/card_3.svg"
+      "image": "../../assets/images/cards/card_3.svg",
+      "alt": "elevator rope" 
     },
      {
       "title": "Стальной канат для лифта GRS 9X21F(9X17)-IWRC 1570",
       "amount": 50,
-      "image": "../assets/images/cards/card_1.svg"
+      "image": "../../assets/images/cards/card_1.svg",
+      "alt": "elevator rope" 
     },
      {
       "title": "Стальной канат для лифта GRS 8X19W-CWC 1570",
       "amount": 50,
-      "image": "../assets/images/cards/card_4.svg"
+      "image": "../../assets/images/cards/card_4.svg",
+      "alt": "elevator rope" 
     },
      {
       "title": "Купить плоский тяговый ремень для лифта GRS AAA717AJ1(8 кордов)",
       "amount": 50,
-      "image": "../assets/images/cards/card_2.svg"
+      "image": "../../assets/images/cards/card_2.svg",
+      "alt": "elevator belt" 
     }
   ]
--- js/catalog.js
+++ js/catalog.js
@@ -1,8 +1,69 @@
-import { cards } from "../cards";
+import { cards } from "../cards.js";
 
-const catalogList = document.querySelector('catalog-list');
-const drawCard = (card) => {
-  let cardItem = document.createElement('li');
+const getCard = (card) => {
+  const cardItem = document.createElement('li');
+  cardItem.classList.add('catalog-list__item');
   
+  const img = document.createElement('img');
+  img.classList.add('item-img');
+  img.src = card.image;
+  img.alt = card.alt;
 
+  const content = document.createElement('div');
+  content.classList.add('item-content');
+
+  const title = document.createElement('p');
+  title.classList.add('item-title');
+  title.innerText = card.title;
+
+  const contentBottom = document.createElement('content-bottom');
+  contentBottom.classList.add('content-bottom');
+
+  const instock = document.createElement('div');
+  instock.classList.add('instock');
+
+  let instockMarkerPath = '';
+  let instockTextValue = '';
+
+  if (card.amount > 0) {
+    instockMarkerPath = '../../assets/images/instock.svg';
+    instockTextValue = `В наличии ${card.amount} м.`;
+  } else {
+    instockMarkerPath = '../../assets/images/out-of-stock.svg';
+    instockTextValue = 'Под заказ';
+  }
+
+  const instockImg = document.createElement('img');
+  instockImg.classList.add('instock-img');
+  instockImg.src = instockMarkerPath;
+  instockImg.alt = 'instock marker';
+
+  const instockTxt = document.createElement('span');
+  instockTxt.classList.add('instock-txt'); 
+  instockTxt.innerText = instockTextValue;
+
+  const itemBtn = document.createElement('button');
+  itemBtn.classList.add('btn');
+  itemBtn.innerText = 'Подробнее';
+
+  cardItem.appendChild(img);
+  cardItem.appendChild(content);
+
+  content.appendChild(title);
+  content.appendChild(contentBottom);
+
+  contentBottom.appendChild(instock);
+  contentBottom.appendChild(itemBtn);
+
+  instock.appendChild(instockImg);
+  instock.appendChild(instockTxt);
+
+  return cardItem;
 }
+
+const catalogList = document.querySelector('.catalog-list');
+console.log(catalogList);
+
+cards.forEach(card => {
+  catalogList.appendChild(getCard(card));
+});
