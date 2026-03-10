7590865e06c83b66b8bebd0e8bf8ddb6ae39dff5
Shakhrii <sh.ramaldanova@gmail.com>
2026-03-11 01:48:44 +0300

    fix: add resized menu active

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,34 @@
+
 const body = document.querySelector('body');
 const menuBtn = document.querySelector('.menu-burger__btn');
 const menu = document.querySelector('.menu-list');
 const menuItems = Array.from(document.querySelectorAll('.menu-list__item')); 
 const elements = [menuBtn].concat(menuItems);
 
-elements.forEach(function (element) {
-    element.addEventListener('click', function() {
-        menu.classList.toggle('menu-active');
-        menuBtn.classList.toggle('menu-active');
-        body.classList.toggle('menu-active');
-    });
+const mediaQuery = window.matchMedia('(max-width: 1160px)');
+const menuClickHandler = () => {
+  menu.classList.toggle('menu-active');
+  menuBtn.classList.toggle('menu-active');
+  body.classList.toggle('menu-active');
+}
+
+function handleScreenChange(event) {
+  if (event.matches) {
+    elements.forEach(element => {
+    element.addEventListener('click', menuClickHandler);
 });
+  } else {
+    menu.classList.remove('menu-active');
+    menuBtn.classList.remove('menu-active');
+    body.classList.remove('menu-active');
+    elements.forEach(element => {
+    element.removeEventListener('click', menuClickHandler);
+});
+  }
+}
+
+mediaQuery.addEventListener('change', handleScreenChange);
+
+handleScreenChange(mediaQuery);
+
+
